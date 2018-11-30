69dc3ddfd745b61db265bc5b327dc32ecbee3981
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-11-30 03:47:49 +0200

    cleanup and lint main app file

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  console.log('req:', req.headers);
-  console.log('req:', req.headers['x-forwarded-for']);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  console.log(`Visitor: ${req.headers["x-forwarded-for"]}`);
   res.end(req.headers['x-forwarded-for']);
-
 });
 
 const port = process.env.PORT || 8080;
