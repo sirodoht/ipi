be5a257c02ae8c4dbe0789a5eb17224b5b94b726
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-11-30 03:56:56 +0200

    cache ip on constant

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const ip = req.headers['x-forwarded-for'];
+  console.log(`Visitor: ${ip}`);
   res.writeHead(200, { "Content-Type": "text/plain" });
-  console.log(`Visitor: ${req.headers["x-forwarded-for"]}`);
-  res.end(req.headers['x-forwarded-for']);
+  res.end(ip);
 });
 
 const port = process.env.PORT || 8080;
